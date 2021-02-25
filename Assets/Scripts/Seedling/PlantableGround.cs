da5daa4d0ede33b1c43d15e03c6a4cdb7f8fcd1c
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2021-02-25 01:24:45 +0100

    Add Seedlings template. Update project version to Unity 2020.2.6f1

---
+++ Assets/Scripts/Seedling/PlantableGround.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantableGround : MonoBehaviour, IClickable
+{
+	public void Click()
+	{
+		throw new System.NotImplementedException();
+	}
+}
